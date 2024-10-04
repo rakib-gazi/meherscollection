ade00b35fae554b613278f2da35bbcfb839197b4
Rakib Gazi <gazicourses@gmail.com>
2024-10-04 22:36:07 +0600

    php file added

---
+++ php/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./*.php",
+    "./dashboard/**/*.php",
+    "./sign-up/**/*.php",
+    "./login/**/*.php",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'color-logo' :'#132749',
+      },
+      fontFamily:{
+        'nunito' : '"Nunito Sans", sans-serif',
+        'siliguri' : '"Hind Siliguri", sans-serif',
+        'bangla' : '"AdorshoLipi", sans-serif',
+      },
+    },
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
+
